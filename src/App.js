440a9f9836505f51feb363f3973c61e1e9606b9d
WintBree <u.duulatov@gmail.com>
2024-06-01 17:02:48 +0600

    error handled

--- src/App.js
+++ src/App.js
@@ -1,43 +1,95 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      console.log(event.key);
+      playSound(event.key.toUpperCase());
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
+  }, []);
+
+  const drumPads = [
+    {
+      keyCode: 81,
+      text: "Q",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    },
+    {
+      keyCode: 87,
+      text: "W",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+    },
+    {
+      keyCode: 69,
+      text: "E",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+    },
+    {
+      keyCode: 65,
+      text: "A",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+    },
+    {
+      keyCode: 83,
+      text: "S",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+    },
+    {
+      keyCode: 68,
+      text: "D",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+    },
+    {
+      keyCode: 90,
+      text: "Z",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+    },
+    {
+      keyCode: 88,
+      text: "X",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+    },
+    {
+      keyCode: 67,
+      text: "C",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+    },
+  ];
+
+  const playSound = (id) => {
+    const audio = document.getElementById(id);
+    console.log(audio)
+    if (audio) {
+            audio.play();
+    }
+  };
+
   return (
-    <div className="drum-container">
-      <div className="keys-container">
-        <div className="grid-item">
-          <button>Button 1</button>
+    <div className="App">
+      <div id="drum-machine">
+        <div id="display"></div>
+        <div className="drum-pads">
+          {drumPads.map((drumPad) => (
+            <div
+              key={drumPad.keyCode}
+              onClick={() => playSound(drumPad.text)}
+              className="drum-pad"
+              id={drumPad.src}
+            >
+              {drumPad.text}
+              <audio
+                src={drumPad.src}
+                id={drumPad.text}
+                className="clip"
+              ></audio>
+            </div>
+          ))}
         </div>
-        <div className="grid-item">
-          <button>Button 2</button>
-        </div>
-        <div className="grid-item">
-          <button>Button 3</button>
-        </div>
-        <div className="grid-item">
-          <button>Button 4</button>
-        </div>
-        <div className="grid-item">
-          <button>Button 5</button>
-        </div>
-        <div class="grid-item">
-          <button>Button 6</button>
-        </div>
-        <div className="grid-item">
-          <button>Button 7</button>
-        </div>
-        <div className="grid-item">
-          <button>Button 8</button>
-        </div>
-        <div className="grid-item">
-          <button>Button 9</button>
-        </div>
-      </div>
-      <div className="controls-container">
-      <div class="volume-controller">
-        <input type="range" min="0" max="100" value="50" class="volume-slider" id="volumeSlider" />
-        <div class="volume-level" id="volumeLevel">Volume: 50%</div>
-    </div>
       </div>
     </div>
   );
